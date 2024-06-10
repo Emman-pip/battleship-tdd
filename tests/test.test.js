35fa15dba10034ed5c6f1dac05f9ad2c9ac46f08
Emman-pip <parrenoemmanuel756@gmail.com>
2024-06-10 13:34:16 +0800

    Add tests for the ship factory.

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,3 +1,24 @@
-test("setup test", () => {
-  expect(1).toBe(1);
+const { Ship } = require("../src/index");
+
+describe("#Ship factory", () => {
+  it("Ship exists.", () => {
+    expect(Ship).not.toBeUndefined();
+  });
+  const ship = Ship(2);
+  // length, #hits, is sunk
+  it("has getLength", () => {
+    expect(ship.getLength).not.toBeUndefined();
+  });
+  it("has number of hits", () => {
+    expect(ship.hits).not.toBeUndefined();
+  });
+  it("has isSunkMethod", () => {
+    expect(ship.isSunk).not.toBeUndefined();
+  });
+  it("get length works", () => {
+    for (let i = 0; i < 5; i++) {
+      const sTest = Ship(i);
+      expect(sTest.getLength()).toEqual(i);
+    }
+  });
 });
